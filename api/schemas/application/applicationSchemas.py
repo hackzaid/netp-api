9d8cf89cf269bb2984ac446dc9ae365e12521df9
Hack Zaid <donatius2008@hotmail.com>
2023-01-19 10:32:47 +0300

    Member Application Feature v1.0

--- api/schemas/application/applicationSchemas.py
+++ api/schemas/application/applicationSchemas.py
@@ -8,12 +8,10 @@ from apifairy.fields import FileField
 
 class ApplicationSchema(ma.SQLAlchemySchema):
     class Meta:
-        model = MemberApplication
         ordered = True
+        model = MemberApplication
 
-    id = ma.auto_field(dump_only=True)
     regNo = ma.Integer(dump_only=True)
-    memberID = ma.Integer(required=True)
     productID = ma.Integer(required=True)
     businessType = ma.String(required=True)
     companyDescription = ma.String(required=True)
@@ -22,5 +20,3 @@ class ApplicationSchema(ma.SQLAlchemySchema):
     companyProfile = FileField(required=True)
     exportNo = ma.String(required=True)
 
-    member = ma.Nested('MemberSchema', dump_only=True)
-    majorProduct = ma.Nested('ProductSchema', dump_only=True)
